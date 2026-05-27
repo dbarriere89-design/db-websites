3ca4775fbb93a79cbdc1f137e918a668c936f5da
dbarriere89-design <dbarriere89@hotmail.com>
2026-05-27 18:09:37 +1000

    Update social preview and allow Meta crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,28 @@ import type { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "Facebot",
+        allow: "/",
+      },
+      {
+        userAgent: "meta-externalagent",
+        allow: "/",
+      },
+      {
+        userAgent: "meta-externalfetcher",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://dbwebsites.au/sitemap.xml",
     host: "https://dbwebsites.au",
   }
